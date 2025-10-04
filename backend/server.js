02e4f3696cde5f07510139ff6a8ab28d4551a77c
fullstackdevarchon <fullstackdev.archon@gmail.com>
2025-10-04 23:22:40 +0530

    labour fix

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173", // Vite frontend
+      "http://localhost:5173", // Vite frontend added
       "http://localhost:5175",
       "http://localhost:5176",
       "http://localhost:5174",
